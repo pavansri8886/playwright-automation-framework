b3cfec28857e0d921d051ade84e147863892a3b3
pavansri8886 <65497622+pavansri8886@users.noreply.github.com>
2026-03-26 11:17:24 +0100

    cicd - test2, fixed full screen issue-v6

--- pages/reportGenPage.ts
+++ pages/reportGenPage.ts
@@ -6,42 +6,20 @@ export class ReportGenPage {
     constructor(private page: Page) { }
 
     //this will take you from my files to report generation page by selecting type of report
-
     async createReport() {
-        const floatBtn = this.page.locator('.ant-float-btn-body');
-        const menuBtn = this.page.getByRole(
-            reportGenLocators.createBTN.role,
-            { name: reportGenLocators.createBTN.name }
-        );
-
-        const isFloatVisible = await floatBtn.isVisible();
-
-        if (isFloatVisible) {
-            await floatBtn.click();
-        } else {
-            await menuBtn.click();
+        const createButton = await this.page.getByRole(reportGenLocators.createBTN.role, { name: reportGenLocators.createBTN.name }).isVisible();
+        if (createButton==true){
+            await this.page.getByRole(reportGenLocators.createBTN.role, { name: reportGenLocators.createBTN.name }).click();
+        }else{
+            await this.page.getByText('more').click();
+            await this.page.getByRole(reportGenLocators.createBTN.role, { name: reportGenLocators.createBTN.name }).click();
         }
-
-        await this.page.getByRole(
-            reportGenLocators.documentBTN.role,
-            { name: reportGenLocators.documentBTN.name }
-        ).click();
-
-        await this.page.getByRole(
-            reportGenLocators.templateSearchBar.role,
-            { name: reportGenLocators.templateSearchBar.name }
-        ).click();
-
-        await this.page.getByRole('option', {
-            name: testData.typeOfReport, exact: true
-        }).click();
-
-        await this.page.getByRole(
-            reportGenLocators.okBTN.role,
-            { name: reportGenLocators.okBTN.name }
-        ).click();
+        await this.page.getByRole(reportGenLocators.documentBTN.role, { name: reportGenLocators.documentBTN.name }).click();
+        await this.page.getByRole(reportGenLocators.templateSearchBar.role, { name: reportGenLocators.templateSearchBar.name }).click();
+        await this.page.getByRole('option', { name: testData.typeOfReport, exact: true }).click();
+        await this.page.getByRole(reportGenLocators.okBTN.role, { name: reportGenLocators.okBTN.name }).click();
     }
-
+    
     //ICCR - Invasive Carcinoma - To capture details of given page
     async captureDetails() {
         await this.page.getByRole('textbox').first().fill(testData.GivenName);
